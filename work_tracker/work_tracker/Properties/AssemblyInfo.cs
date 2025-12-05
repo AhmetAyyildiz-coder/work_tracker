4d78611b60f245a6a1b1c0f9248e6c60cf399b77
ahmetayyildiz <ahmetayyildiz@A409>
2025-12-05 14:33:09 +0300

    AssemblyInfo ve proje dosyasında güncellemeler yapıldı; başlık, açıklama ve uygulama revizyonu güncellendi.

--- work_tracker/work_tracker/Properties/AssemblyInfo.cs
+++ work_tracker/work_tracker/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("work_tracker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Work Tracker")]
+[assembly: AssemblyDescription("İş Akışı Yönetim Aracı - Kanban, Scrum, Toplantı ve Zaman Takibi")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("work_tracker")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCompany("ARPAS")]
+[assembly: AssemblyProduct("Work Tracker")]
+[assembly: AssemblyCopyright("Copyright © 2025 ARPAS")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
